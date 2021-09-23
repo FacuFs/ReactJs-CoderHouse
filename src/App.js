6ef15adc9db5238cffc670e6842179781148bec5
FacuFs <Fs.Facu@yahoo.com>
2021-09-23 12:59:24 -0300

    V0.0.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './componentes/NavBar.js';
+import ItemListContainer from "./componentes/ItemListContainer.js"
 
 function App() {
   return (
 
     <div className="App">
       <NavBar/>
+      <ItemListContainer titulo="Nuestros productos"/>
     </div>
   );
 }
---
+++ src/componentes/ItemListContainer.js
@@ -0,0 +1,10 @@
+
+
+const ItemListContainer = (props) =>{
+    const titulo= <h1>{props.titulo}</h1>
+    return (
+            titulo
+        )
+}
+
+export default ItemListContainer;
