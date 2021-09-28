9d36ff18fc91c181f02bef25b3732f4f9b1368e0
FacuFs <Fs.Facu@yahoo.com>
2021-09-28 17:12:35 -0300

    V0.0.5

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './componentes/NavBar.js';
 import ItemListContainer from "./componentes/ItemListContainer.js"
+import ItemCount from "./componentes/ItemCount"
 
 function App() {
+  const stock=10;
   return (
 
     <div className="App">
       <NavBar/>
       <ItemListContainer titulo="Nuestros productos"/>
+      <ItemCount stock={stock} inicial="1"/>
     </div>
   );
 }
---
+++ src/componentes/ItemCount.js
@@ -0,0 +1,48 @@
+const { useState } = require("react")
+
+
+const ItemCount = ({stock, inicial})=>{
+    const [contador, setContador]=useState(inicial);
+    const [stockRestante, setStock]= useState(stock)
+    
+    const sumarCont = ()=>{
+        if (contador<stockRestante) {
+            setContador(parseInt(contador)+1);
+        }
+    }
+    const restarCont = ()=>{
+        if (contador>1) {
+            setContador(contador-1);
+        }
+    }
+
+    const comprar =()=>{
+        setStock(parseInt(stockRestante)-contador)
+        alert('Usted a comprado '+contador+' unidades del producto');
+    }
+
+
+
+
+    return(
+        <div className="containerContador">
+            <p>Contador</p>
+            <p>Stock: {stockRestante}</p>
+            <div className="containerContador__control">
+                <div onClick={restarCont}>
+                    <p>-</p>
+                </div>
+                <div className="contador">
+                    <p> {contador}</p>
+                </div>
+                <div onClick={sumarCont}>
+                    <p>+</p>
+                </div>
+            </div>
+            <button onClick={comprar}>Agregar al carrito</button>
+
+        </div>
+    )
+}
+
+export default ItemCount;
